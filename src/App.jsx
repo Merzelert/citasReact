5e81a73e31062580b0c510e9c3bcd80292749b47
Erik Ivan Retana Lopez <merzelert@gmail.com>
2023-01-31 17:05:41 -0600

    localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { ListadoPacientes } from './components/ListadoPacientes'
 
 function App() {
 
-    const [pacientes, setPacientes] = useState([])
+    const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? [])
     const [paciente, setPaciente] = useState({}) // editar pacientes
 
     useEffect(()=>{ // local storage
